ea0838f4526f4525fa10c6192aac8212f6aa61ef
Angelica0918 <20243ds136@utez.edu.mx>
2025-10-22 22:05:44 -0600

    Hicimos la estructura basica del MVVM

---
+++ app/src/main/java/com/example/musicplayer/data/model/Cancion.kt
@@ -0,0 +1,13 @@
+package com.example.musicplayer.data.model
+
+import androidx.annotation.DrawableRes
+
+data class Cancion (
+    val id:Int,
+    val nombre: String,
+    val album: String,
+    val artista: String,
+    val descripcion: String,
+    @DrawableRes val image: Int
+
+)
---
+++ app/src/main/java/com/example/musicplayer/viewmodel/Prueba.kt
@@ -0,0 +1,4 @@
+package com.example.musicplayer.viewmodel
+
+class Prueba {
+}
