c66a10fe7cee4ce8630b018b2b2f2272a34cb678
Monserrattorress <20243ds147@utez.edu.mx>
2025-10-24 01:26:51 -0600

    Agregue el RecupContraScreen y RecupContraViewModel

---
+++ app/src/main/java/com/example/musicplayer/ui/screen/RecupContraScreen.kt
@@ -0,0 +1,54 @@
+package com.example.musicplayer.ui.screen
+
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxHeight
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.unit.dp
+import com.example.musicplayer.ui.components.Button
+import com.example.musicplayer.ui.components.InputField
+import com.example.musicplayer.ui.components.TextField
+import com.example.musicplayer.ui.components.TextoSubtitulo
+import com.example.musicplayer.ui.components.TextoTitulo
+
+@Composable
+fun RecupContraScreen(viewModel: RecupContraViewModel, navController: NavController) {
+    Column(
+        modifier = Modifier
+            .fillMaxSize()
+            .fillMaxWidth()
+            .fillMaxHeight()
+            .padding(30.dp),
+        horizontalAlignment = Alignment.CenterHorizontally,
+        verticalArrangement = Arrangement.spacedBy(20.dp, Alignment.CenterVertically)
+    ) {
+        TextoTitulo("Recuperar Contraseña")
+
+        InputField(
+            viewModel = viewModel,
+            label = "Correo"
+        )
+
+        InputField(
+            viewModel = viewModel,
+            label = "Nueva Contraseña"
+        )
+
+        InputField(
+            viewModel = viewModel,
+            label = "Confirmar Contraseña"
+        )
+
+        Button ("Aceptar") {
+            viewModel.RecupContra {
+                navController.navigate("recupcontra") {
+                    popUpTo("login") { inclusive = true } // Evita volver al login
+                }
+            }
+        }
+    }
